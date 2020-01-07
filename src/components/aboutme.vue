2dcbc8e301e9c77f6c00cbb2291fb740b64f21a1
Mikee Rose I. Bersabal <mribersabal@vauldex.com>
2020-01-07 12:20:33 +0800

    Update about me

--- src/components/aboutme.vue
+++ src/components/aboutme.vue
@@ -1,19 +1,14 @@
 <template>
   <div class="about-me">
-    <!-- <span class="title">About Me</span>
-    <p class="description"> 
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-    </p> -->
     <div class="about-article">
       <div class="article-container">
         <div class="about-content">
           <span class="title">About Me</span>
           <p class="description">
-            I enjoy learning new stuff and solving problems. I started professionally coding in 2016 and have since cultivated skills in using various programming tools for both my backend and frontend projects. 
+            I enjoy learning and solving problems. I started professionally coding in 2016 and have since cultivated skills in using various programming tools for both my backend and frontend projects. 
           </p>
           <p class="description">
-            So far, I've used Scala, Java, Webpack, VueJS, NodeJS, SASS and HTML5 as tools in building software. I'm constantly learning new tools to add to this list.
+            So far, I've used Scala and Java for backend, and Webpack, VueJS, NodeJS, SASS and HTML5 for frontend, as tools in building software. I'm constantly learning new technology to add to this list.
           </p>
         </div>
        
--- src/components/menu.vue
+++ src/components/menu.vue
@@ -3,7 +3,6 @@
     <ul>
       <li><a href="#home">home</a></li>
       <li><a href="#about">about</a></li>
-      <li><a href="#contact">contact</a></li>
     </ul>
   </div>
 </template>
