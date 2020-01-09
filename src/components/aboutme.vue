211c5b872b66b79a778c41d7e82a3ac2b30b6c02
Mikee Rose I. Bersabal <mribersabal@vauldex.com>
2020-01-09 12:16:46 +0800

    Update about me

--- src/components/aboutme.vue
+++ src/components/aboutme.vue
@@ -1,25 +1,23 @@
 <template>
   <div class="about-me">
-    <!-- <div class="description"> -->
-      <div class="description">
-        <div class="content">
-          <span class="title">About Me</span>
-          <p>
-            I enjoy learning and solving problems. I started professionally coding in 2016 and have since cultivated skills in using various programming tools for both my backend and frontend projects. 
-          </p>
-          <p>
-            So far, I've used Scala and Java for backend, and Webpack, VueJS, NodeJS, SASS and HTML5 for frontend, as tools in building software. I'm constantly learning new technology to add to this list.
-          </p>
-        </div>
+    <div class="description">
+      <div class="content">
+        <span class="title">About Me</span>
+        <p>
+          I enjoy learning new technology and solving problems. I started professionally coding in 2016 and have since cultivated skills in using various programming tools for both my backend and frontend projects. 
+        </p>
+        <p>
+          So far, I've used Scala and Java for backend, and Webpack, VueJS, NodeJS, SASS and HTML5 for frontend, as tools in building software. I'm continuously studying to add more to this list.
+        </p>
       </div>
-      <div class="work">
-        <div class="content">
-          <span class="title">My work</span>
-         
-        </div>
-       
+    </div>
+    <div class="work">
+      <div class="content">
+        <span class="title">My work</span>
+        
       </div>
-    <!-- </div> -->
+      
+    </div>
   </div>
 </template>
 
