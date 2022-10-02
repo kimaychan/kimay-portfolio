dde21eba4b97182d66af55c635fe917a8b0ee9e9
kimaychan <mikeebersabal@gmail.com>
2022-10-02 15:15:03 +0800

    Add responsive styling for home

--- components/organisms/kp-home/index.vue
+++ components/organisms/kp-home/index.vue
@@ -4,17 +4,21 @@
       <p>Mikee</p>
       <p>2022 Portfolio</p>
     </header>
-    <img
-      class="home-banner"
-      :src="`${prefix}/images/self-portrait.png`"
-      :srcset="`${prefix}/images/self-portrait.png`"
-      alt="Self Portrait"
-    />
-    <div class="greetings">
-      <h1> Hi, </h1>  
-      <p>
-        I'm a <span class="highlight">Software Developer/</span> bookworm/cat lover from Cebu, Philippines
-      </p>
+    <div class="content-layout">
+      <div class="banner-container">
+        <img
+          class="home-banner"
+          :src="`${prefix}/images/self-portrait.png`"
+          :srcset="`${prefix}/images/self-portrait.png`"
+          alt="Self Portrait"
+        />
+      </div>
+      <div class="greetings">
+        <h1> Hi, </h1>  
+        <p>
+          I'm a <span class="highlight">Software Developer/</span> bookworm/cat lover from Cebu, Philippines
+        </p>
+      </div>
     </div>
   </section>
 </template>
@@ -45,37 +49,28 @@ header {
   padding: 20px 40px;
 }
 
-.home {
-  // background: $vista-white;
-  text-align: center;
-  // height: 100vh;
-}
-
-.home-banner {
-  height: 680px;
-  display: inline-block;
-  vertical-align: top;
+.content-layout {
+  height: calc(100vh - 71.5px);
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
 }
 
 .greetings {
   color: $regular;
-  position: relative;
-  display: inline-block;
   text-align: left;
-  vertical-align: bottom;
-  height: calc(850px - 100px);
+  height: 100%;
   width: 400px;
+  display: flex;
+  flex-direction: column;
   h1 {
     margin: 0;
-    font-size: 138px;
-    position: absolute;
-    top: 150px;
+    font-size: 9em;
   }
   p {
-    font-size: 32px;
+    font-size: 2em;
     width: 400px;
-    position: absolute;
-    top: 330px;
     font-weight: bold;
   }
 }
@@ -83,5 +78,29 @@ header {
 .highlight {
   background: $powder-blue;
   padding: 5px 10px;
+}
+
+@media only screen and (min-width: $breakpoint-med){
+  .greetings {
+    margin-left: 40px;
+    justify-content: center;
+  }
+}
+
+@media only screen and (max-width: $breakpoint-med){ 
+  .home-banner {
+    height: 60vh;
+    margin-top: 2rem;
+  }
+}
+
+@media only screen and (max-width: $breakpoint-small) {
+  .greetings {
+    margin-top: 3rem;
+    align-items: center;
+    p {
+      text-align: center;
+    }
+  }
 }
 </style>
